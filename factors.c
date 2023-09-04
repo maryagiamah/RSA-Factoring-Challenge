edc4571c122de6d3abd0f0e88be991009b5f6d77
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-09-04 04:09:39 +0100

    Update factors.c

--- factors.c
+++ factors.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void factorize(unsigned long int n) {
-    for (long unsigned int i = 2; i <= n; i++) {
-        while (n % i == 0) {
-            printf("%lu=%lu*%lu\n", n, i, n / i);
-            return;
-        }
-    }
+void factorize(unsigned long int n)
+{
+	long unsigned int i;
+	for (i = 2; i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			printf("%lu=%lu*%lu\n", n, i, n / i);
+			return;
+		}
+	}
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <file>\n", argv[0]);
-        return 1;
-    }
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Usage: %s <file>\n", argv[0]);
+		return 1;
+	}
 
     FILE *file = fopen(argv[1], "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("Error opening file");
         return 1;
     }
 
     char buffer[256];
-    while (fgets(buffer, sizeof(buffer), file)) {
+    while (fgets(buffer, sizeof(buffer), file)) 
+    {
         unsigned long int num = atoi(buffer);
         factorize(num);
     }

d7b5c0f19acd51273fb05547b11a4aca88133892
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-09-04 03:54:33 +0100

    Update factors.c

--- factors.c
+++ factors.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void factorize(unsigned long int n) {
-    for (int i = 2; i <= n; i++) {
+    for (long unsigned int i = 2; i <= n; i++) {
         while (n % i == 0) {
             printf("%lu=%lu*%lu\n", n, i, n / i);
             return;

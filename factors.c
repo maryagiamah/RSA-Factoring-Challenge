f9542a9db10d98646ee176a9e5b19e9ba03173a3
Agiamah Mary Uyoyooghene <maryagiamah@gmail.com>
2023-09-04 03:51:47 +0100

    Update factors.c

--- factors.c
+++ factors.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void factorize(int n) {
+void factorize(unsigned long int n) {
     for (int i = 2; i <= n; i++) {
         while (n % i == 0) {
             printf("%d=%d*%d\n", n, i, n / i);
